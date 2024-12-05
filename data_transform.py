07aa8959aa651086eb24aa5260ab8b3983d925e3
LuisLozanoAD <luis.lozano.ad.gmail.com>
2024-12-05 17:35:19 -0500

    Eliminación de datos nulos y duplicados en el script

--- data_transform.py
+++ data_transform.py
@@ -5,20 +5,26 @@ import os
 archivo_csv = 'produccion.csv'
 
 try:
-     # Verificar si el archivo existe
+    # Verificar si el archivo existe
     if not os.path.exists(archivo_csv):
         raise FileNotFoundError(f"El archivo {archivo_csv} no existe.")
     
     # Leer el archivo CSV
     data = pd.read_csv(archivo_csv)
     
-    # Ordenar los datos por nombre
-    data_ordenada = data.sort_values(by='Fecha')
+    # Eliminar filas con valores nulos
+    data_limpia = data.dropna()
+    
+    # Eliminar filas duplicadas
+    data_limpia = data_limpia.drop_duplicates()
+    
+    # Ordenar los datos por fecha
+    data_ordenada = data_limpia.sort_values(by='Fecha')
     
     # Exportar a Excel
-    archivo_excel = 'produccion_ordenada.xlsx'
+    archivo_excel = 'produccion_ordenada_limpia.xlsx'
     data_ordenada.to_excel(archivo_excel, index=False)
     
-    print(f"Datos exportados exitosamente a {archivo_excel}")
+    print(f"Datos transformados exitosamente a {archivo_excel}")
 except Exception as e:
     print(f"Error al transformar los datos: {e}")
